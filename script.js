1b1ad95b63546f46501b0963f02a6ef5c641ea51
dave-code03 <davecode03@gmail.com>
2026-06-20 15:45:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ const themeBtn = document.querySelector(".theme-btn");
 themeBtn.onclick = () => {
 document.body.classList.toggle("light");
 }
+
+const nav = document.querySelector("nav");
+
+
+window.addEventListener("scroll",()=>{
+
+
+if(window.scrollY > 50){
+
+nav.classList.add("scrolled");
+
+}
+
+else{
+
+nav.classList.remove("scrolled");
+
+}
+
+
+});
