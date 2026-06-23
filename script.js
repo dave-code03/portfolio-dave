25eb877f1e24df1ccdcb159c611b818cb178b3d1
dave-code03 <davecode03@gmail.com>
2026-06-23 10:15:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -31,14 +31,12 @@ nav.classList.remove("scrolled");
 });
 
 
-
 const menuIcon = document.querySelector(".menu-icon");
-
 const navLinks = document.querySelector(".nav-links");
 
 
-menuIcon.onclick = ()=>{
+menuIcon.addEventListener("click", ()=>{
 
 navLinks.classList.toggle("active");
 
-};
+});
