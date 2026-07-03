3899b88a6e832f3d951e3b58122377017439a485
dave-code03 <davecode03@gmail.com>
2026-07-03 15:48:44 -0400

    Update script.js

--- restaurant/script.js
+++ restaurant/script.js
@@ -23,3 +23,19 @@ icon.classList.add("fa-bars");
 }
 
 });
+
+const backToTop = document.querySelector(".back-to-top");
+
+window.addEventListener("scroll",()=>{
+
+    if(window.scrollY>300){
+
+        backToTop.classList.add("show");
+
+    }else{
+
+        backToTop.classList.remove("show");
+
+    }
+
+});
