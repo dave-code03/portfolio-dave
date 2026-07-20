6a5ecd7dd47ac49f4c6e5d4269a1fba90169ad65
Dave Occeant <davecode03@gmail.com>
2026-07-20 00:50:56 -0400

    Implement loader functionality on page load
    Add loader functionality to hide the loader on page load.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+/* ==========================================
+   PORTFOLIO MAIN JAVASCRIPT
+========================================== */
 
+/* ==========================================
+   LOADER
+========================================== */
+
+window.addEventListener("load", () => {
+
+    const loader = document.getElementById("loader");
+
+    loader.classList.add("loader-hidden");
+
+});
