ed06b3217283d2d0b768120b86facb2233ddf58e
Dave Occeant <davecode03@gmail.com>
2026-07-21 11:57:36 -0400

    Initialize Dave Dev Portfolio in main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+"use strict";
+
+console.log("Dave Dev Portfolio initialized.");
+
 /* ==========================================
    PORTFOLIO MAIN JAVASCRIPT
 ========================================== */
