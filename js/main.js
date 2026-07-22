540ced278e009067b2427f212693c41c3ea965ef
Dave Occeant <davecode03@gmail.com>
2026-07-21 20:40:58 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,39 +1,6 @@
 "use strict";
 
 /* ==========================================
-   ELEMENT REFERENCES
+   DAVE DEV PORTFOLIO
+   Main JavaScript File
 ========================================== */
-
-const loader = document.querySelector(".loader");
-const backToTop = document.getElementById("backToTop");
-
-
-/* ==========================================
-   FUNCTIONS
-========================================== */
-
-function hideLoader() {
-
-    if (!loader) return;
-
-    loader.classList.add("hidden");
-
-}
-
-
-/* ==========================================
-   INITIALIZATION
-========================================== */
-
-window.addEventListener("load", () => {
-
-    setTimeout(() => {
-
-        hideLoader();
-
-    }, 800);
-
-});
-
-
-console.log("Dave Dev Portfolio initialized.");
